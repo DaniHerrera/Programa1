72fd0a19c07f706aa10a2357a2184ec2e7cdbb07
DaniHerrera <danielherreracubells@gmail.com>
2023-03-29 13:35:42 +0200

    Mi segundo commit

--- src/programa1/Programa1.java
+++ src/programa1/Programa1.java
@@ -1,13 +1,61 @@
+
 package programa1;
 
+/**
+ *
+ * @author dani
+ */
+
+//****** Atajos *****
+//sout
+//soutv //última variable definida
+
 public class Programa1 {
-    
+
+   
     public static void main(String[] args) {
         
-        byte edad = 127;
+        short numeroPlantas = 125;
+    
+        boolean flag = true;
         
-        System.out.println("la edad es: " + edad);
+        String nombre = "pepe";
         
+        System.out.println("flag = " + flag);
+        
+        System.out.println("El número deplantas es de:" + numeroPlantas);
+        
+        int a = 10;
+        a++; // a = a +1;
+        ++a; // a = a +1;
+        System.out.println("a = " + (a));
+        
+        
+        int x=4;
+        int y= 2;
+        
+        if (x%y ==1){
+            System.out.println("es impar" );
+        }
+        else{
+            System.out.println("es par");
+         
+        }
+        
+        
+        int x2 = 8;
+        int y2= 3;
+        
+        if (x2%y2 ==1){
+                        System.out.println("es impar" );
+        }
+        else{
+                System.out.println("es par");
+        }
+
+
+       
     }
     
 }
+
